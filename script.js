fcdfd4c4f7471634b160ab4e25972b6c82243744
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-05-29 00:19:08 -0500

    feat: button to change size & fix starter square container bug

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const body = document.querySelector("body");
 const divContainer = document.createElement("div");
-divContainer.className = "container";
+const btn = document.createElement("button");
+let divSquareNum = 16;
+let divSquareNumSquared = 256;
+let divSquareNumRemover = 0;
 
+body.appendChild(btn);
 body.appendChild(divContainer);
 
-for (let i = 0; i < 256; i++) {
-	const fragmentDivSquare = document.createDocumentFragment();
-	const divSquare = document.createElement("div");
-	divSquare.className = "square";
-	fragmentDivSquare.appendChild(divSquare);
-	divContainer.appendChild(fragmentDivSquare);
+divContainer.className = "container";
+
+btn.textContent = "Size";
+
+function defineContainerSize(divSquareNumSquared) {
+	for (let i = 0; i < divSquareNumSquared; i++) {
+		const fragmentDivSquare = document.createDocumentFragment();
+		const divSquare = document.createElement("div");
+		divSquare.className = "square";
+		fragmentDivSquare.appendChild(divSquare);
+		divContainer.appendChild(fragmentDivSquare);
+	}
 }
+
+defineContainerSize(divSquareNumSquared);
 
 divContainer.addEventListener("mouseover", (event) => {
 	event.target.style.backgroundColor = "#000";
+});
+
+btn.addEventListener("click", () => {
+	divSquareNum = prompt("What number of squares for the side-length of the grid?");
+	while (divSquareNumRemover < 256) {
+		// TODO: find a way to access divSquare to mass delete without breaking the defineContainerSize function
+		divSquare.remove();
+		divSquareNumRemover++; 
+	}
+	divSquareNumSquared = divSquareNum * divSquareNum;
+	defineContainerSize(divSquareNumSquared);
+
 });
